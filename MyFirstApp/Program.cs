f6f5ed67377727004a4ee5dededca0a9821edb87
RasmusR <rasmus.raadik@tktk.ee>
2023-02-09 19:25:47 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter you name:");
+string userName = Console.ReadLine();
+Console.WriteLine("Hello" + ", " + userName + "!");
+Console.WriteLine($"Hello, {userName} !");
